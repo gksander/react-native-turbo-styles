7f8af128f8997ab45ff0a12a62a5c890dbffc22f
Grant Sander <gksander93@gmail.com>
2021-05-02 22:14:31 -0500

    New builder!

--- lib/createStyleBuilder.test.ts
+++ lib/createStyleBuilder.test.ts
@@ -1,112 +1,193 @@
 import { createStyleBuilder } from "./createStyleBuilder";
 
+const makeBasicBuilder = () =>
+  createStyleBuilder({
+    sizing: { sm: 4, md: 8, lg: 12 },
+    colors: {
+      red: "#f00",
+      blue: "rgb(0, 0, 255)",
+    },
+    bgOpacities: {
+      10: 0.1,
+      50: 0.5,
+      95: 0.95,
+    },
+    borderSizes: {
+      1: 1,
+      2: 2,
+      3: 3,
+      4: 4,
+    },
+  }).builder;
+
 describe("createStyleBuilder", () => {
-  it("creates a one-dimensional style", () => {
-    const { builder } = createStyleBuilder({
-      m: {
-        __propertiesToSet: ["margin"],
-        foo: 8,
-      },
-      p: {
-        __propertiesToSet: ["padding"],
-        bar: "10%",
-      },
-    });
+  it("handles margin/padding with constraint", () => {
+    const builder = makeBasicBuilder();
 
-    expect(builder("m-foo")).toEqual({ margin: 8 });
-    expect(builder("p-bar")).toEqual({ padding: "10%" });
-    expect(builder("m-foo", "p-bar")).toEqual({ margin: 8, padding: "10%" });
+    expect(builder("m-sm")).toEqual({ margin: 4 });
+    expect(builder("mx-md")).toEqual({ marginLeft: 8, marginRight: 8 });
+    expect(builder("mb-sm")).toEqual({ marginBottom: 4 });
+
+    expect(builder("p-sm")).toEqual({ padding: 4 });
+    expect(builder("px-md")).toEqual({ paddingLeft: 8, paddingRight: 8 });
+    expect(builder("pb-sm")).toEqual({ paddingBottom: 4 });
   });
 
-  it("overwrites styles in order passed", () => {
-    const { builder } = createStyleBuilder({
-      m: {
-        __propertiesToSet: ["margin"],
-        1: 1,
-        2: 2,
-      },
-    });
+  it("handles margin with override", () => {
+    const builder = makeBasicBuilder();
 
-    expect(builder("m-1", "m-2")).toEqual({ margin: 2 });
-    expect(builder("m-2", "m-1")).toEqual({ margin: 1 });
+    expect(builder("m-[32]")).toEqual({ margin: 32 });
+    expect(builder("mx-[10%]")).toEqual({
+      marginLeft: "10%",
+      marginRight: "10%",
+    });
   });
 
-  it("sets multiple properties with a single value", () => {
-    const { builder } = createStyleBuilder({
-      mx: {
-        __propertiesToSet: ["marginLeft", "marginRight"],
-        foo: 8,
-      },
-    });
-
-    expect(builder("mx-foo")).toEqual({ marginLeft: 8, marginRight: 8 });
+  it("handles inset/positioning", () => {
+    const builder = makeBasicBuilder();
+    expect(builder("top-sm")).toEqual({ top: 4 });
   });
 
-  it("--bg-opacity to set background opacity", () => {
-    const { builder } = createStyleBuilder({
-      bg: {
-        __propertiesToSet: ["backgroundColor"],
-        red: "#f00",
-        blue: "rgb(0,0,255)",
-      },
-      "bg-opacity": {
-        __propertiesToSet: ["--bg-opacity"],
-        50: 0.5,
-      },
-    });
+  it("handles background color", () => {
+    const builder = makeBasicBuilder();
+    expect(builder("bg-red")).toEqual({ backgroundColor: "#f00" });
+    expect(builder("bg-[blue]")).toEqual({ backgroundColor: "blue" });
+  });
 
+  it("handles background opacity", () => {
+    const builder = makeBasicBuilder();
     expect(builder("bg-red", "bg-opacity-50")).toEqual({
       backgroundColor: "rgba(255, 0, 0, 0.5)",
     });
-    // TODO:
-    expect(builder("bg-blue", "bg-opacity-50")).toEqual({
-      backgroundColor: "rgba(0, 0, 255, 0.5)",
-    });
   });
 
-  it("should set multiple properties if multiple values provided", () => {
-    const { builder } = createStyleBuilder({
-      mx: {
-        __propertiesToSet: ["marginLeft", "marginRight"],
-        base: 8,
-        uneven: [4, 12],
-      },
-      text: {
-        __propertiesToSet: ["fontSize", "lineHeight"],
-        lg: [24, 1.7],
-      },
-    });
-
-    expect(builder("mx-base")).toEqual({ marginLeft: 8, marginRight: 8 });
-    expect(builder("mx-uneven")).toEqual({ marginLeft: 4, marginRight: 12 });
-    expect(builder("text-lg")).toEqual({ fontSize: 24, lineHeight: 1.7 });
+  it("handles relative/absolute position", () => {
+    const builder = makeBasicBuilder();
+    expect(builder("relative")).toEqual({ position: "relative" });
+    expect(builder("absolute")).toEqual({ position: "absolute" });
   });
 
-  it("allows single override values", () => {
-    const { builder } = createStyleBuilder({
-      m: {
-        __propertiesToSet: ["margin"],
-        base: 8,
-      },
-      mx: {
-        __propertiesToSet: ["marginLeft", "marginRight"],
-        base: 5,
-      },
-    });
-
-    expect(builder("m-[32]")).toEqual({ margin: 32 });
-    expect(builder("m-[10%]")).toEqual({ margin: "10%" });
-    expect(builder("mx-[5]")).toEqual({ marginLeft: 5, marginRight: 5 });
+  it("handles border widths", () => {
+    const builder = makeBasicBuilder();
+    expect(builder("border-2")).toEqual({ borderWidth: 2 });
   });
 
-  it("allows multiple override values", () => {
-    const { builder } = createStyleBuilder({
-      mx: {
-        __propertiesToSet: ["marginLeft", "marginRight"],
-        base: 8,
-      },
-    });
-
-    expect(builder("mx-[4,12]")).toEqual({ marginLeft: 4, marginRight: 12 });
+  it("handles overflow", () => {
+    const builder = makeBasicBuilder();
+    expect(builder("overflow-hidden")).toEqual({ overflow: "hidden" });
+    expect(builder("overflow-scroll")).toEqual({ overflow: "scroll" });
   });
 });
+
+// import { createStyleBuilder } from "./createStyleBuilder";
+//
+// describe("createStyleBuilder", () => {
+//   it("creates a one-dimensional style", () => {
+//     const { builder } = createStyleBuilder({
+//       m: {
+//         __propertiesToSet: ["margin"],
+//         foo: 8,
+//       },
+//       p: {
+//         __propertiesToSet: ["padding"],
+//         bar: "10%",
+//       },
+//     });
+//
+//     expect(builder("m-foo")).toEqual({ margin: 8 });
+//     expect(builder("p-bar")).toEqual({ padding: "10%" });
+//     expect(builder("m-foo", "p-bar")).toEqual({ margin: 8, padding: "10%" });
+//   });
+//
+//   it("overwrites styles in order passed", () => {
+//     const { builder } = createStyleBuilder({
+//       m: {
+//         __propertiesToSet: ["margin"],
+//         1: 1,
+//         2: 2,
+//       },
+//     });
+//
+//     expect(builder("m-1", "m-2")).toEqual({ margin: 2 });
+//     expect(builder("m-2", "m-1")).toEqual({ margin: 1 });
+//   });
+//
+//   it("sets multiple properties with a single value", () => {
+//     const { builder } = createStyleBuilder({
+//       mx: {
+//         __propertiesToSet: ["marginLeft", "marginRight"],
+//         foo: 8,
+//       },
+//     });
+//
+//     expect(builder("mx-foo")).toEqual({ marginLeft: 8, marginRight: 8 });
+//   });
+//
+//   it("--bg-opacity to set background opacity", () => {
+//     const { builder } = createStyleBuilder({
+//       bg: {
+//         __propertiesToSet: ["backgroundColor"],
+//         red: "#f00",
+//         blue: "rgb(0,0,255)",
+//       },
+//       "bg-opacity": {
+//         __propertiesToSet: ["--bg-opacity"],
+//         50: 0.5,
+//       },
+//     });
+//
+//     expect(builder("bg-red", "bg-opacity-50")).toEqual({
+//       backgroundColor: "rgba(255, 0, 0, 0.5)",
+//     });
+//     // TODO:
+//     expect(builder("bg-blue", "bg-opacity-50")).toEqual({
+//       backgroundColor: "rgba(0, 0, 255, 0.5)",
+//     });
+//   });
+//
+//   it("should set multiple properties if multiple values provided", () => {
+//     const { builder } = createStyleBuilder({
+//       mx: {
+//         __propertiesToSet: ["marginLeft", "marginRight"],
+//         base: 8,
+//         uneven: [4, 12],
+//       },
+//       text: {
+//         __propertiesToSet: ["fontSize", "lineHeight"],
+//         lg: [24, 1.7],
+//       },
+//     });
+//
+//     expect(builder("mx-base")).toEqual({ marginLeft: 8, marginRight: 8 });
+//     expect(builder("mx-uneven")).toEqual({ marginLeft: 4, marginRight: 12 });
+//     expect(builder("text-lg")).toEqual({ fontSize: 24, lineHeight: 1.7 });
+//   });
+//
+//   it("allows single override values", () => {
+//     const { builder } = createStyleBuilder({
+//       m: {
+//         __propertiesToSet: ["margin"],
+//         base: 8,
+//       },
+//       mx: {
+//         __propertiesToSet: ["marginLeft", "marginRight"],
+//         base: 5,
+//       },
+//     });
+//
+//     expect(builder("m-[32]")).toEqual({ margin: 32 });
+//     expect(builder("m-[10%]")).toEqual({ margin: "10%" });
+//     expect(builder("mx-[5]")).toEqual({ marginLeft: 5, marginRight: 5 });
+//   });
+//
+//   it("allows multiple override values", () => {
+//     const { builder } = createStyleBuilder({
+//       mx: {
+//         __propertiesToSet: ["marginLeft", "marginRight"],
+//         base: 8,
+//       },
+//     });
+//
+//     expect(builder("mx-[4,12]")).toEqual({ marginLeft: 4, marginRight: 12 });
+//   });
+// });
--- lib/createStyleBuilder.ts
+++ lib/createStyleBuilder.ts
@@ -1,67 +1,157 @@
-import { ImageStyle, TextStyle, ViewStyle } from "react-native";
+import { FlexStyle, TextStyle, ViewStyle } from "react-native";
 import { colorStringToRgb } from "./colorStringToRgb";
 
-const cleanMaybeNumberString = (val: string): string | number =>
-  /^\d+$/.test(val) ? Number(val) : val;
+type Constraints = {
+  sizing: Record<string | number, string | number>;
+  colors: Record<string | number, string>;
+  bgOpacities: Record<string | number, number>;
+  borderSizes: Record<string | number, number>;
+};
 
 type NonSymbol<T> = Exclude<T, symbol>;
 type ValueOf<T> = T[keyof T];
 
-type ThemeConfig = {
-  [key: string]: {
-    // TODO: This shouldn't be `any`. Should match up with __propertiesToSet
-    [key: string]: any;
-    __propertiesToSet: (
-      | keyof ViewStyle
-      | keyof TextStyle
-      | keyof ImageStyle
-      | "--bg-opacity"
-    )[];
-  };
-};
-
 /**
- * Style builder
+ * Create style builder. Pass in constraints, create handler
  */
-export const createStyleBuilder = <
-  C extends ThemeConfig,
-  P extends {
-    [key in keyof C]: `${NonSymbol<key>}-${
-      | NonSymbol<keyof Omit<C[key], "__propertiesToSet">>
-      | `[${string}]`}`;
-  }
->(
-  config: C
-) => {
-  const builder = (...args: ValueOf<P>[]) => {
+export const createStyleBuilder = <C extends Constraints>(constraints: C) => {
+  type sizeInput = NonSymbol<keyof C["sizing"]> | `[${string}]`;
+  const getSizeValue = (val: sizeInput): string | number | undefined => {
+    return constraints.sizing[val] ?? extractFromBrackets(val);
+  };
+  const sizeHandler = (...properties: Array<keyof FlexStyle>) => (
+    key: sizeInput
+  ) => {
+    const val = getSizeValue(key);
+    // S TODO: Improve this?
+    return properties.reduce<FlexStyle>((acc, prop) => {
+      // @ts-ignore
+      acc[prop] = val;
+      return acc;
+    }, {});
+  };
+
+  type colorInput = NonSymbol<keyof C["colors"]> | `[${string}]`;
+  const getColorValue = (val: colorInput): string | undefined => {
+    return constraints.colors[val] ?? extractFromBrackets(val);
+  };
+  const colorHandler = (...properties: Array<keyof TextStyle>) => (
+    inp: colorInput
+  ) => {
+    const val = getColorValue(inp);
+    // S TODO: Improve this?
+    return properties.reduce<TextStyle>((acc, prop) => {
+      // @ts-ignore
+      acc[prop] = val;
+      return acc;
+    }, {});
+  };
+
+  // Border sizing
+  type borderSizeInput = NonSymbol<keyof C["borderSizes"]> | `[${string}]`;
+  const getBorderSizeValue = (
+    val: borderSizeInput
+  ): string | number | undefined => {
+    return constraints.borderSizes[val] ?? extractFromBrackets(val);
+  };
+  const borderSizeHandler = (...properties: Array<keyof ViewStyle>) => (
+    inp: borderSizeInput
+  ) => {
+    const val = getBorderSizeValue(inp);
+    // S TODO: Improve this?
+    return properties.reduce<ViewStyle>((acc, prop) => {
+      // @ts-ignore
+      acc[prop] = val;
+      return acc;
+    }, {});
+  };
+
+  const config = {
+    // Margin
+    m: sizeHandler("margin"),
+    mx: sizeHandler("marginLeft", "marginRight"),
+    my: sizeHandler("marginTop", "marginBottom"),
+    ml: sizeHandler("marginLeft"),
+    mr: sizeHandler("marginRight"),
+    mt: sizeHandler("marginTop"),
+    mb: sizeHandler("marginBottom"),
+    // Padding
+    p: sizeHandler("padding"),
+    px: sizeHandler("paddingLeft", "paddingRight"),
+    py: sizeHandler("paddingTop", "paddingBottom"),
+    pl: sizeHandler("paddingLeft"),
+    pr: sizeHandler("paddingRight"),
+    pt: sizeHandler("paddingTop"),
+    pb: sizeHandler("paddingBottom"),
+    // Inset/position
+    inset: sizeHandler("top", "bottom", "left", "right"),
+    "inset-x": sizeHandler("left", "right"),
+    "inset-y": sizeHandler("top", "bottom"),
+    left: sizeHandler("left"),
+    right: sizeHandler("right"),
+    top: sizeHandler("top"),
+    bottom: sizeHandler("bottom"),
+    // Sizing
+    w: sizeHandler("width"),
+    "min-w": sizeHandler("minWidth"),
+    "max-w": sizeHandler("maxWidth"),
+    h: sizeHandler("height"),
+    "min-h": sizeHandler("minHeight"),
+    "max-h": sizeHandler("maxHeight"),
+    // Colors
+    bg: colorHandler("backgroundColor"),
+    "border-color": colorHandler("borderColor"),
+    // Background opacity
+    "bg-opacity": (inp: NonSymbol<keyof C["bgOpacities"]> | `[${number}]`) => {
+      const val = constraints.bgOpacities[inp] ?? extractFromBrackets(inp);
+      return { "--bg-opacity": val };
+    },
+    relative: () => <FlexStyle>{ position: "relative" },
+    absolute: () => <FlexStyle>{ position: "absolute" },
+    // Border sizing
+    border: borderSizeHandler("borderWidth"),
+    "border-t": borderSizeHandler("borderTopWidth"),
+    "border-b": borderSizeHandler("borderBottomWidth"),
+    "border-l": borderSizeHandler("borderLeftWidth"),
+    "border-r": borderSizeHandler("borderRightWidth"),
+    // Overflow
+    overflow: (overflow: NonNullable<ViewStyle["overflow"]>) =>
+      <ViewStyle>{
+        overflow,
+      },
+  } as const;
+
+  /**
+   * Our actual builder. Takes an array of "class names" that
+   * 	are constructed from our config
+   */
+  const builder = (
+    ...args: Array<
+      ValueOf<
+        {
+          [k in keyof typeof config]: Parameters<
+            typeof config[k]
+          >[0] extends undefined
+            ? `${NonSymbol<k>}`
+            : `${NonSymbol<k>}-${NonSymbol<Parameters<typeof config[k]>[0]>}`;
+        }
+      >
+    >
+  ) => {
     let styles = {} as Record<string | number, any>;
 
     for (let c of args) {
       const m = c.match(/^(.+)-(.+)$/);
       const prop = m?.[1];
       const value = m?.[2];
-      const pertinentCfg = prop ? config?.[prop] : null;
 
-      if (prop && value && pertinentCfg) {
-        const partInsideBracket = value.match(/^\[(.*)\]$/)?.[1];
-
-        const val = (() => {
-          if (partInsideBracket) {
-            return partInsideBracket
-              .split(",")
-              .map((bit) => cleanMaybeNumberString(bit.trim()));
-          } else {
-            return pertinentCfg?.[value];
-          }
-        })();
-
-        if (val !== undefined && val !== null) {
-          const isValArray = Array.isArray(val);
-          for (const i in pertinentCfg.__propertiesToSet) {
-            const p = pertinentCfg.__propertiesToSet[i];
-            styles[p] = isValArray ? (val.length === 1 ? val[0] : val[i]) : val;
-          }
-        }
+      if (prop && value) {
+        const handler = config?.[prop as NonSymbol<keyof typeof config>];
+        // @ts-ignore
+        styles = { ...styles, ...handler(value) };
+      } else {
+        // @ts-ignore
+        styles = { ...styles, ...(config?.[c]() || {}) };
       }
     }
 
@@ -75,5 +165,27 @@ export const createStyleBuilder = <
     return styles;
   };
 
-  return { builder, config };
+  return { builder };
+};
+
+/**
+ * If string looks like number, make it number
+ */
+const cleanMaybeNumberString = (val: string): string | number =>
+  /^\d+$/.test(val) ? Number(val) : val;
+
+/**
+ * Extract value from brackets, e.g. [32] -> 32
+ */
+const extractFromBrackets = (
+  val: string | number
+): string | number | undefined => {
+  const insideBracket = (typeof val === "string" ? val : String(val)).match(
+    /^\[(.*)\]$/
+  )?.[1];
+  if (insideBracket) {
+    return cleanMaybeNumberString(insideBracket);
+  } else {
+    return undefined;
+  }
 };
