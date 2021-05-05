dae06bb78a90438ffc0145f7d77ec7ad93655ac7
Grant Sander <gksander93@gmail.com>
2021-05-05 16:17:11 -0500

    Remove docs intro

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -19,7 +19,7 @@ module.exports = {
       items: [
         {
           type: "doc",
-          docId: "intro",
+          docId: "getting-started",
           position: "left",
           label: "Documentation",
         },
